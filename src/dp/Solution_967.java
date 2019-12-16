7fde613b61186078f118eaf8bd2b82a071c61cfe
aqxyjay <aqxyjay@gmail.com>
2019-12-16 20:53:05 +0800

    implement 967

--- src/dp/Solution_967.java
+++ src/dp/Solution_967.java
@@ -1,50 +1,33 @@
 package dp;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * https://leetcode-cn.com/problems/numbers-with-same-consecutive-differences/
  */
 public class Solution_967 {
+    private List<Integer> ans = new ArrayList<>();
+
     public int[] numsSameConsecDiff(int N, int K) {
-        List<Integer> ans = new ArrayList<>();
-        Map<Integer, List<Integer>> kNumMap = getKNumMap(K);
+        if (N == 1) {
+            return new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        }
         for (int i = 1; i <= 9; i++) {
-            ans.addAll(getNums(i, N, kNumMap));
+            getNums(i, i, N - 1, K);
         }
         return ans.stream().mapToInt(Integer::intValue).toArray();
     }
 
-    private Map<Integer, List<Integer>> getKNumMap(int k) {
-        Map<Integer, List<Integer>> map = new HashMap<>();
-        for (int i = 0; i < 10; i++) {
-            map.put(i, new ArrayList<>());
+    private void getNums(int num, int pre, int n, int k) {
+        if (n <= 0) {
+            ans.add(num);
+            return;
         }
-        for (int i = 0; i < 10; i++) {
-            if (i < k) {
-                map.get(i).add(i + k);
-            } else {
-                map.get(i).add(i - k);
+        for (int i = 0; i <= 9; i++) {
+            if (Math.abs(pre - i) == k) {
+                getNums(num * 10 + i, i, n - 1, k);
             }
         }
-        return map;
-    }
-
-    private List<Integer> getNums(int i, int n, Map<Integer, List<Integer>> kNumMap) {
-        List<Integer> nums = new ArrayList<>();
-        int depth = 0;
-        int num = i;
-        while (depth < n) {
-            int result = (int) (num * Math.pow(10, n));
-            List<Integer> neighbors = kNumMap.get(num);
-            for (int neighbor : neighbors) {
-
-            }
-            depth++
-        }
-        return null;
     }
 }
